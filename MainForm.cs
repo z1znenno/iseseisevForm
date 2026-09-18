71b76b6e60074d769cfa624b3ccb8746a9a29a24
opilane <opilane@HP-CZC2349HTM>
2026-09-18 10:22:04 +0300

    Add project files.

---
+++ MainForm.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualBasic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace iseseisevForm
+{
+    public partial class MainForm : Form
+    {
+        Button PictureView, mathQuiz, matchingGame;
+
+        public static Font font = new Font("Segoe Script", 9F, FontStyle.Bold);
+        public MainForm()
+        {
+            InitializeComponent();
+            Height = 500;
+            Width = 500;
+            BackgroundImage = Image.FromFile(@"..\..\taustaPiltMain.jpg");
+            Text = "Three form chooser";
+            
+
+            Button mathQuiz = new Button();
+            Button PictureView = new Button();
+            Button matchingGame = new Button();
+
+            PictureView.Text = "Galery";
+            PictureView.Font = font;
+            PictureView.Location = new Point(110, 50);
+            PictureView.Click += PictureView_Click;
+            Controls.Add(PictureView);
+
+            mathQuiz.Text = "Math Quiz";
+            mathQuiz.Font = font;
+            mathQuiz.Location = new Point(185, 50);
+            mathQuiz.Click += MathQuiz_Click;
+            Controls.Add(mathQuiz);
+
+            matchingGame.Text = "Matching Game";
+            matchingGame.Font = font;
+            matchingGame.Width = 125;
+            matchingGame.Click += MatchingGame_Click;
+            matchingGame.Location = new Point(260, 50);
+
+            Controls.Add(matchingGame);
+
+
+        }
+
+        private void MatchingGame_Click(object sender, System.EventArgs e)
+        {
+            MatchingGameForm matchingGameForm = new MatchingGameForm();
+            matchingGameForm.Show();
+        }
+
+        private void MathQuiz_Click(object sender, System.EventArgs e)
+        {
+            MathQuizForm mathQuizForm = new MathQuizForm();
+            mathQuizForm.Show();
+        }
+
+        private void PictureView_Click(object sender, System.EventArgs e)
+        {
+            PictureViewForm pictureViewForm = new PictureViewForm();
+            pictureViewForm.Show();
+        }
+    }
+}
---
+++ PictureViewForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iseseisevForm
+{
+    public partial class PictureViewForm : Form
+    {
+        PictureBox picture;
+        Button elPilt, jargPilt, taustCol, sule, lisaPilt;
+        OpenFileDialog openFile;
+        List<string> images = new List<string>();
+        int pilt = 0;
+
+        public PictureViewForm()
+        {
+            InitializeComponent();
+            Text = "Galery";
+            Height = 700;
+            Width = 700;
+
+            for (int i = 1; i <= 7; i++)
+            {
+                images.Add($@"..\..\pildid\image{i}.jpg");
+            }
+
+            picture = new PictureBox();
+            picture.Size = new Size(500, 500);
+            picture.Left = 90;
+            picture.Image = Image.FromFile(images[pilt]);
+            Controls.Add(picture);
+
+            Button lisaPilt = new Button();
+            lisaPilt.Text = "Lisa pilt";
+            lisaPilt.Font = MainForm.font;
+            lisaPilt.Top = 550;
+            lisaPilt.Left = 90;
+            Controls.Add(lisaPilt);
+
+            Button elPilt = new Button();
+            elPilt.Text = "Eelmine pilt";
+            elPilt.Font = MainForm.font;
+            elPilt.Top = 550;
+            elPilt.Left = 165;
+            elPilt.Click += (s, e) => 
+            {
+                try
+                {
+                    pilt -= 1;
+                }
+                catch
+                {
+                    
+                }
+            };
+            Controls.Add(elPilt);
+
+            Button jargPilt = new Button();
+            jargPilt.Text = "Jargmine pilt";
+            jargPilt.Font = MainForm.font;
+            jargPilt.Top = 550;
+            jargPilt.Left = 240;
+            jargPilt.Click += (s, e) =>
+            {
+                try
+                {
+                    pilt -= 1;
+                }
+                catch
+                {
+
+                }
+            };
+            Controls.Add(jargPilt);
+        }
+    }
+}
